007f2e14e797cbed306feb538fe8f84ef6b2bde6
smg-927 <godholy927@gmail.com>
2025-05-29 22:00:25 +0900

    탄성체 테스트
    ㅈㄱㄴ

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Diagnostics;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        StartCoroutine(LoadSceneCoroutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            // 여기서 로딩 진행률을 처리할 수 있습니다.
+            yield return null;
+        }
+
+        switch(sceneName)
+        {
+            case "MainScreen":
+                //GameManager.Instance.MenuReady();
+                break;
+            case "StageSelect":
+                //GameManager.Instance.StageSelectReady();
+                break;
+            case "Ingame":
+                //GameManager.Instance.MappingReady(true);
+                break;
+        }
+    }
+}
